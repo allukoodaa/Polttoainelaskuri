4fcebf5049c6c690b906f23543b548306e28b149
Aleksi Saari <alesaari@gmail.com>
2022-11-11 12:42:33 +0200

    Add some error handling

--- laskuri_v2.py
+++ laskuri_v2.py
@@ -13,9 +13,14 @@ Syötä litrahinta tankatessa (€/l):
 
     pituus = float(input("Syötä matkan pituus:\n"))
     kustannus = (pituus / 100) * kulutus * litrahinta
-    kyytilaiset = int(input("Syötä kyytiläisten määrä:\n"))
+    kyytilaiset = input("Syötä kyytiläisten määrä:\n")
 
-    if kyytilaiset > 0:
+    if kyytilaiset:
+        try:
+            kyytilaiset = int(kyytilaiset)
+        except ValueError:
+            print("Tapahtui virhe. Kyytiläisten määrän tulee olla kokonaislukus.")
+            exit()
         kustannus = kustannus / (kyytilaiset + 1)
         kustannus = round(kustannus, 2)
         print(f"Matkan polttoainekustannus on {kustannus} euroa per lärvi.")
